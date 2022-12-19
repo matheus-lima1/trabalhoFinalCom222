c3de4cc520f32bee90430a94eb8dc5579560436f
Pedro Lucas Guerra de Lima <pedrolucas.guerra28@gmail.com>
2022-12-18 23:23:40 -0300

    Mudando metodo set do mongoose

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', true);
 mongoose.connect('mongodb+srv://matheuslima1:trabalhofinalcom222@cluster0.qlgtymm.mongodb.net/?retryWrites=true&w=majority');
 mongoose.Promise = global.Promise;
 
