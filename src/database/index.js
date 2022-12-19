d56705338935fd97cb101e9cf9af4e1d52e2e2cc
Pedro Lucas Guerra de Lima <pedrolucas.guerra28@gmail.com>
2022-12-19 14:50:39 -0300

    teste

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', false);
 mongoose.connect('mongodb+srv://matheuslima1:trabalhofinalcom222@cluster0.qlgtymm.mongodb.net/?retryWrites=true&w=majority');
 mongoose.Promise = global.Promise;
 
