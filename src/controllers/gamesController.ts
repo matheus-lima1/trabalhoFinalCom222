75c1bd048a4a353d52cb903a6d8b7a9c4eaa365e
Pedro Lucas Guerra de Lima <pedrolucas.guerra28@gmail.com>
2022-12-19 14:16:01 -0300

    Corrigindo erro de tipagem

--- src/controllers/gamesController.ts
+++ src/controllers/gamesController.ts
@@ -25,7 +25,7 @@ class GameController {
       return response.status(401).send({ error: scheme });
     }
 
-    jwt.verify(token, authConfig.secret, (err, decoded) => {
+    jwt.verify(token, authConfig.secret, (err: any, decoded: { id: any; }) => {
       if (err) return response.status(401).send({ error: 'Token Invalido' });
 
       request.body.userId = decoded.id;
